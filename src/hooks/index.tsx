6fabbf7d92444d3f80dc5edf2118d8177518bb57
Djamilson <djamilson@gmail.com>
2021-02-25 00:53:13 -0300

    init

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { AuthProvider } from './auth';
 import { LoadingProvider } from './loading';
 import { ToastProvider } from './toast';
 
 const AppProvider: React.FC = ({ children }) => {
   return (
-    <AuthProvider>
-      <LoadingProvider>
-        <ToastProvider>{children}</ToastProvider>
-      </LoadingProvider>
-    </AuthProvider>
+    <LoadingProvider>
+      <ToastProvider>{children}</ToastProvider>
+    </LoadingProvider>
   );
 };
 export default AppProvider;
